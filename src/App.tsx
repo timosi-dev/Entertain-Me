1a7fb5d30dff85fe7b64ad11eab351a696bdf286
siharulize <broouwnie@gmail.com>
2024-07-23 17:48:54 +0200

    added .env and connection to TMDB Database

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,30 @@ import MoviesPage from './pages/MoviesPage'
 import ShowsPage from './pages/ShowsPage'
 import EntertainPage from './pages/EntertainPage'
 
+
 function App() {
+  
+    //Fetch data from TMDB API
+  const fetchAPIData = async (endpoint: string): Promise<T> => {
+    const API_KEY = process.env.API_KEY;
+    const API_URL = process.env.API_URL;
+
+    //showSpinner();
+
+    const response = await fetch (`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+    const data = await response.json();
+
+    //hideSpinner();
+    return data;
+  }
+
+  const displayPopularMovies = async (): Promise<void> => { 
+    const results = await fetchAPIData('movie/popular');
+    console.log(results.results[0].backdrop_path)
+  }
+
+  displayPopularMovies();
+
 
   const router = createBrowserRouter(
     createRoutesFromElements(
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+// // https://vitejs.dev/config/
+// export default defineConfig(
+//   {
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//   }
+// })
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+      'process.env.API_URL': JSON.stringify(env.API_URL)
+    },
+    plugins: [react()],
   }
 })
